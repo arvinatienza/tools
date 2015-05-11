c9b6a671a965c790d1e11a22372581a542db2c35
Arvin Atienza <arvin.atienza@gmail.com>
2015-05-11 10:07:36 -0500

    .

---
+++ docker/apache2-php5/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Xdebug configuration
+if [ $APPLICATION_ENV = "Development" ]
+then
+    if ! grep -Fxq "xdebug.enable" /etc/php5/conf.d/xdebug.ini
+    then
+        config="
+           xdebug.enable=Off
+           xdebug.remote_host=localhost
+           xdebug.remote_port=9000
+           xdebug.remote_handler=dbgp
+        ";
+        echo $config >> /etc/php5/conf.d/xdebug.ini;
+    fi;
+    sed -i "s/_enable=Off/_enable=On/g" /etc/php5/conf.d/xdebug.ini;
+fi;
+
+# initialize
+service apache2 restart
+tail -f /var/log/apache2/error.log
