3fdda164e9244e56e65827b3707aa5afb81c8bba
Arvin Atienza <arvin.atienza@gmail.com>
2017-09-16 12:59:59 +0000

    .

---
+++ dockerfiles/dev-base/get-vim-plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd ~/.janus
+
+git clone https://github.com/leafgarland/typescript-vim.git
+git clone https://github.com/evidens/vim-twig.git
+git clone https://github.com/tpope/vim-liquid.git
+
+cd -
+
+exit 1;
